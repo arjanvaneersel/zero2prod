fa622de45afc581bbbfaca93cca3b534ae606535
Arjan van Eersel <arjanvaneersel@gmail.com>
2023-05-18 15:33:54 +0300

    Implement connection pool and random db names for tests

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
 use std::net::TcpListener;
+use sqlx::PgPool;
 use zero2prod::{startup::run, configuration::get_configuration};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     let configuration = get_configuration().expect("Failed to read configuration.");
+    let connection = PgPool::connect(
+        &configuration.database.connection_string()
+    )
+    .await
+    .expect("Failed to connect to Postgres.");
+
     let listener = TcpListener::bind(format!("0.0.0.0:{}", configuration.application_port)).expect("Failed to bind random port");
-    run(listener)?.await
+    run(listener, connection)?.await
 }
--- src/startup.rs
+++ src/startup.rs
@@ -1,15 +1,19 @@
 use std::net::TcpListener;
 
 use actix_web::{dev::Server, web, App, HttpServer};
+use sqlx::PgPool;
 
 use crate::routes::{health_check, subscribe};
 
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
+pub fn run(listener: TcpListener, connection: PgPool) -> Result<Server, std::io::Error> {
+    // Wrap the connection in a smart pointer
+    let connection = web::Data::new(connection);
     Ok(
-        HttpServer::new(|| 
+        HttpServer::new(move || 
             App::new()
                 .route("/health_check", web::get().to(health_check))
                 .route("/subscriptions", web::post().to(subscribe))
+                .app_data(connection.clone())
         )
         .listen(listener)?
         .run()
