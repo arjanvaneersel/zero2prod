a53c40b22b2e51d2c5ef4a141d6e49ae40b4ca28
Arjan van Eersel <arjanvaneersel@gmail.com>
2023-05-18 14:43:44 +0300

    Prepare database integration

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 use std::net::TcpListener;
-
-use zero2prod::run;
+use zero2prod::{startup::run, configuration::get_configuration};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind random port");
+    let configuration = get_configuration().expect("Failed to read configuration.");
+    let listener = TcpListener::bind(format!("0.0.0.0:{}", configuration.application_port)).expect("Failed to bind random port");
     run(listener)?.await
 }
